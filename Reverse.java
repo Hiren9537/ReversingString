d65ede583118a6c05a1e490c0ac0b6166f3bebba
Hiren9537 <trivedi.hiren123@gmail.com>
2020-09-02 13:28:20 +0530

    first commit

---
+++ Reverse.java
@@ -0,0 +1,32 @@
+public class Reverse 
+{
+	public static void main(String args[])
+	{	
+	
+		String sentences;
+		sentences = "This is a coding test. I like java so much. This is my day";
+		String[] paragraph = sentences.split("\\.");
+		
+		for (int i = 0 ; i <= paragraph.length -1; i++)
+		{
+			String result = "";
+			result = paragraph[i];
+			
+			String[] w = result.split(" ");
+			String ans1 = "";
+			for (int j = w.length - 3; j >= 0; j--)
+			{
+				ans1 += (w[j] + " ");
+			}
+			System.out.print(ans1.trim());
+			String ans2 = "";
+			for (int k = w.length - 2; k <= w.length - 1 ; k++)
+			{
+				ans2 +=	(" " + w[k]);
+			}
+			System.out.print(ans2 + ". ");
+			
+		}
+
+	}
+}
